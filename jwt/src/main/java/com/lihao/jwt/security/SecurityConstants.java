794869415bfbee0b34dac0945fcb26bf7bf6752f
Hao <roadmonster1989@gmail.com>
2021-02-23 12:48:52 -0800

    added jwt authentication filter

---
+++ jwt/src/main/java/com/lihao/jwt/security/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.lihao.jwt.security;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "Secret";
+    public static final long VALID_SPAN = 864_000_000;
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users/sign-up";
+}
