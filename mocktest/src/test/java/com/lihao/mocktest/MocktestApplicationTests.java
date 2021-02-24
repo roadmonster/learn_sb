8579680db7b1bf935f058a63cb3f5b94a71754b7
Hao <roadmonster1989@gmail.com>
2021-02-23 16:58:08 -0800

    initial commit

---
+++ mocktest/src/test/java/com/lihao/mocktest/MocktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihao.mocktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
